fbffdf9d2f804b4da01ad68824e9b9a8b01cff55
Mikael <dev.kov.mtl@gmail.com>
2022-05-30 16:37:20 -0400

    Lesson 4 - FundMe - fallback() and receive()

--- 02_FundMe/FundMe.sol
+++ 02_FundMe/FundMe.sol
@@ -7,21 +7,26 @@ import "./PriceConverter.sol";
 // Get funds from users
 // Withdraw fund from users
 // set a minimum funding value in USD
+
+// custom error to save gas
+error NotOwner();
+
 contract FundMe {
     // now we can use the function of priceconverter on uint256
     using PriceConverter for uint256; 
 
     // minimum usd we want 
-    uint256 public minimumUSD = 50 * 1e18; 
+    // uint256 public minimumUSD = 50 * 1e18; 
+    uint256 public constant MINIMUM_USD = 50 * 1e18; 
     // keep track of anyone who fund us
     address[] public funders;
     // how much each funders has given
     mapping(address =>  uint256) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     // anybody can call it -> public
@@ -30,7 +35,7 @@ contract FundMe {
         // we want to set a minimum fund amount
         // 1. How to send ETH to this contract
         // require(getConversionRate(msg.value )>= minimumUSD, "Not enough found"); // 1e18 = 1 * 10 **18
-        require(msg.value.getConversionRate() >= minimumUSD, "Not enough money");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Not enough money");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
@@ -38,7 +43,6 @@ contract FundMe {
     // we want to withdraw all the fund from contract
     // reset the funders[] and addressToAmountFunded
     function withdraw() public onlyOwner {
-        require(msg.sender == owner);
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex]; // we get the funder address
             addressToAmountFunded[funder] = 0;
@@ -60,8 +64,21 @@ contract FundMe {
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Sender is not owner");
+        // require(msg.sender == i_owner, "Sender is not owner");
+        if(msg.sender != i_owner){
+            revert NotOwner();
+        }
         _; // doing the rest of code
     }
 
+    // we can add receive() and fallback() just in case somebody 
+    // send money instead of calling the fund() method
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+ 
 }
