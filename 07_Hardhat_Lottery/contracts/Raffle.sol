78d8cd9d54d3458be3d84dffd07f58237ff6ed6a
Mikael <dev.kov.mtl@gmail.com>
2022-06-06 08:39:57 -0400

    Lesson 9 - Hardhat Lottery Smart Contract - ChainLink request random number

--- 07_Hardhat_Lottery/contracts/Raffle.sol
+++ 07_Hardhat_Lottery/contracts/Raffle.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 // What do we want people to do?
 // Enter - Participate in the raffle (buy a ticket)
 // Pick a random winner (verifiable winner)
@@ -11,17 +14,38 @@ pragma solidity ^0.8.7;
 
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     // we are going to set minimum ETH price
     // immutable to save gas
     uint256 private immutable i_entranceFee;
     // keep tracks of all players
     address payable[] private s_players;
+
+    // coordinartor to get the random num
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    // variable for our coordinator requestRandomWords() methods
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
     /* event */
     event RaffleEnter(address indexed player);
+    event RequestRaffleWinner(uint256 indexed requestId);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -34,6 +58,29 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
+    function requestRandomWinner() external {
+        // Chainlink Vrf is a 2 transaction process
+        // request random number
+        // once we get the random number do something
+
+        // Will revert if subscription is not set and funded.
+        // return request id uin32
+        uint256 requestId = i_entranceFee.requestRandomWords(
+            i_gasLane, //gasLane // https://docs.chain.link/docs/vrf-contracts/
+            i_subscriptionId, // subscription to fund our contract //https://vrf.chain.link/
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS // how many random number we want to get
+        );
+        emit RequestRaffleWinner(requestId);
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    /* View / Pure Functions */
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
--- README.md
+++ README.md
@@ -722,3 +722,5 @@ emit storedNumber(
     msg.sender
 );
 ```
+
+(chainlink video explanation: 14:03:05)
