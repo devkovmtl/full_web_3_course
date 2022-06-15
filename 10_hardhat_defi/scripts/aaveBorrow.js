641b169d7859f7876a032f80e5af9d7ba4d17fc4
Mikael <dev.kov.mtl@gmail.com>
2022-06-15 11:51:17 -0400

    Lesson 13 - HardHat Defi - Get Lending pool

--- 10_hardhat_defi/scripts/aaveBorrow.js
+++ 10_hardhat_defi/scripts/aaveBorrow.js
@@ -1,8 +1,35 @@
+const { getNamedAccounts, ethers } = require('hardhat');
 const { getWeth } = require('../scripts/getWeth');
 
 async function main() {
   // protocol treats everything as an ERC20 token
   await getWeth();
+  const { deployer } = await getNamedAccounts();
+  // abi, address
+  // Lending Pool Address Provider:
+  // lending pool address Provider https://docs.aave.com/developers/v/2.0/deployed-contracts/deployed-contracts :
+  // 0xb53c1a33016b2dc2ff3653530bff1848a515c8c5
+  // lending pool
+  const lendingPool = await getLendingPool(deployer);
+  console.log(`Lending pool address ${lendingPool.address}`);
+}
+
+async function getLendingPool(account) {
+  const lendingPoolAddressesProvider = await ethers.getContractAt(
+    'ILendingPoolAddressesProvider',
+    '0xb53c1a33016b2dc2ff3653530bff1848a515c8c5',
+    account
+  );
+  const lendingPoolAddress =
+    await lendingPoolAddressesProvider.getLendingPool();
+
+  // get contract
+  const lendingPool = await ethers.getContractAt(
+    'ILendingPool',
+    lendingPoolAddress,
+    account
+  );
+  return lendingPool;
 }
 
 main()
